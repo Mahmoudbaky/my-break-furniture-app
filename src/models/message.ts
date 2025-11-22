d3a93236dadbb795dd14543cbb2e916b549eef34
MahmoudSaleh <ma7mouudbaky@gmail.com>
2025-11-22 17:06:00 +0300

    Refactor message schema formatting for improved readability

--- src/models/message.ts
+++ src/models/message.ts
@@ -2,23 +2,26 @@ import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
-const messageSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const messageSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  message: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Message", messageSchema);
